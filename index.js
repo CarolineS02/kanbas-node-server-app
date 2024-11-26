6f633518a8542907e1b9fbdec6c2d39c6fec2123
CarolineS02 <103371788+CarolineS02@users.noreply.github.com>
2024-11-26 18:33:09 -0500

    Update index.js to check node server URL

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ import QuestionsRoute from './Kanbas/QuizQuestions/routes.js';
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
-console.log('NETLIFY_URL:', process.env.NETLIFY_URL);
+console.log('NETLIFY_URL:', process.env.NODE_SERVER_DOMAIN);
 
 const app = express();
 app.use(
